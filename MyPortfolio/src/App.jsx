ae729f32ea9fa64bc6f80fb8db61e88764f526df
agareaditya-8055 <adityaagare8055@gmail.com>
2024-02-22 00:49:51 +0530

    Both body and header components has added in App.jsx

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Body from "./components/body/Body";
+import Header from "./components/header/Header";
 
 function App() {
-  return <></>;
+  return (
+    <div className={`container mx-auto px-4 `}>
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
